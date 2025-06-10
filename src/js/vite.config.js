4dc2924ea953edf53d59646ddd0cb9f9c0b89826
Hirakada <hirakada.id@gmail.com>
2025-06-11 01:00:52 +0700

    modified:   src/js/vite.config.js

--- src/js/vite.config.js
+++ src/js/vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/', 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets', 
-
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        projects: resolve(__dirname, 'projects/index.html'),
-        projectDetail: resolve(__dirname, 'projects/project-detail.html'),
-        docs: resolve(__dirname, 'docs/index.html'), 
-        contact: resolve(__dirname, 'contact/index.html'),
-      }
-    }
-  },
-});
+    base: '/',
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        assetsDir: 'assets', 
+        
+        rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            projects: resolve(__dirname, 'projects/index.html'),
+            projectDetail: resolve(__dirname, 'projects/project-detail.html'),
+            docs: resolve(__dirname, 'docs/index.html'), 
+            contact: resolve(__dirname, 'contact/index.html'),
+        },
+        },
+    },
+})
