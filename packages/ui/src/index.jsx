cac29127f872b9c223d71d140409e089523a09dd
Hirakada <hirakada.id@gmail.com>
2025-06-23 00:48:31 +0700

    modified:   apps/hirakada/package.json 	modified:   packages/ui/src/index.jsx

--- packages/ui/src/index.jsx
+++ packages/ui/src/index.jsx
@@ -4,3 +4,4 @@ export { SocialButton, BackButton, OpenButton } from './Buttons/Buttons';
 export { default as ProjectCard } from './Cards/Cards';
 export { default as Header } from './Header/Header';
 export { default as ScrollToTop } from './Scroll/Scroll';
+export { default as Footer } from './Footer/Footer';
