f1b21550f2c44552f0f0af4151c17ce87a7b17c5
Hirakada <hirakada.id@gmail.com>
2025-06-23 11:09:47 +0700

    modified:   apps/hirakada/App.jsx 	modified:   apps/hirakada/package.json 	modified:   apps/portfolio/App.jsx 	modified:   apps/portfolio/package.json 	modified:   pnpm-lock.yaml 	modified:   pnpm-workspace.yaml

--- apps/hirakada/App.jsx
+++ apps/hirakada/App.jsx
@@ -8,6 +8,7 @@ import DocPage from './src/pages/Doc.jsx';
 import JourneyPage from './src/pages/JourneyPage.jsx';
 import './src/index.css';
 import '@hirakada/ui/ui.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
@@ -161,71 +162,74 @@ function App() {
     }
 
     return (
-        <Router basename="/">
-            <ScrollToTop /> 
+        <>
+            <Router basename="/">
+                <ScrollToTop /> 
 
-            {globalLoading && (
-                <div className="flex items-center justify-center w-full fixed top-0 left-0 bg-black z-50 pointer-events-none">
-                    <OrbitProgress color="#E0E0E0" size="medium" text="" textColor="" />
-                </div>
-            )}
+                {globalLoading && (
+                    <div className="flex items-center justify-center w-full fixed top-0 left-0 bg-black z-50 pointer-events-none">
+                        <OrbitProgress color="#E0E0E0" size="medium" text="" textColor="" />
+                    </div>
+                )}
 
-            <Header currentApp="hirakada" /> 
-            <main>
-                <Routes>
-                    <Route 
-                        path="/" 
-                        element={
-                            <Home
-                                projects={allProjects}
-                                attributes={allAttributes}
-                                globalLoading={globalLoading}
-                                firstSectionRef={firstSectionRef} 
-                            />
-                        } 
-                    />
+                <Header currentApp="hirakada" /> 
+                <main>
+                    <Routes>
+                        <Route 
+                            path="/" 
+                            element={
+                                <Home
+                                    projects={allProjects}
+                                    attributes={allAttributes}
+                                    globalLoading={globalLoading}
+                                    firstSectionRef={firstSectionRef} 
+                                />
+                            } 
+                        />
 
-                    <Route 
-                        path="/doc" 
-                        element={
-                            <DocPage
-                                globalLoading={globalLoading}
-                            />
-                        }
-                    />
+                        <Route 
+                            path="/doc" 
+                            element={
+                                <DocPage
+                                    globalLoading={globalLoading}
+                                />
+                            }
+                        />
 
-                    <Route
-                        path="/journey"
-                        element={
-                            <JourneyPage
-                                globalLoading={globalLoading}
-                            />
-                        }
-                    />
+                        <Route
+                            path="/journey"
+                            element={
+                                <JourneyPage
+                                    globalLoading={globalLoading}
+                                />
+                            }
+                        />
 
-                    <Route 
-                        path="*" 
-                        element={
-                            <div
-                                className="
-                                    flex flex-col items-center justify-center
-                                    text-gray-700 bg-gray-50 p-4
-                                "
-                                style={{ minHeight: 'calc(100vh - 64px)' }}
-                            >
-                                <h1 className="text-6xl font-extrabold text-red-600">404</h1>
-                                <p className="text-2xl mt-4">Page Not Found</p>
-                                <p className="mt-2 text-lg">The page you are looking for does not exist.</p>
-                                <Link to="/" className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
-                                    Go to Home
-                                </Link>
-                            </div>
-                        } 
-                    />
-                </Routes>
-            </main>
-            <Footer /> 
-        </Router>
+                        <Route 
+                            path="*" 
+                            element={
+                                <div
+                                    className="
+                                        flex flex-col items-center justify-center
+                                        text-gray-700 bg-gray-50 p-4
+                                    "
+                                    style={{ minHeight: 'calc(100vh - 64px)' }}
+                                >
+                                    <h1 className="text-6xl font-extrabold text-red-600">404</h1>
+                                    <p className="text-2xl mt-4">Page Not Found</p>
+                                    <p className="mt-2 text-lg">The page you are looking for does not exist.</p>
+                                    <Link to="/" className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+                                        Go to Home
+                                    </Link>
+                                </div>
+                            } 
+                        />
+                    </Routes>
+                </main>
+                <Footer /> 
+            </Router>
+            <SpeedInsights/>
+        </>
     );
 }
 
--- apps/portfolio/App.jsx
+++ apps/portfolio/App.jsx
@@ -158,62 +158,65 @@ function App() {
     }
 
     return (
-        <Router basename="/">
-            <ScrollToTop /> 
+        <>
+            <Router basename="/">
+                <ScrollToTop /> 
 
-            {globalLoading && (
-                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black z-50 pointer-events-none">
-                    <OrbitProgress color="#E0E0E0" size="medium" text="" textColor="" />
-                </div>
-            )}
+                {globalLoading && (
+                    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black z-50 pointer-events-none">
+                        <OrbitProgress color="#E0E0E0" size="medium" text="" textColor="" />
+                    </div>
+                )}
 
-            <Header currentApp="portfolio" /> 
-            <main>
-                <Routes>
-                    <Route 
-                        path="/" 
-                        element={
-                            <ProjectPage
-                                projects={allProjects}
-                                attributes={allAttributes}
-                                globalLoading={globalLoading}
-                            />
-                        } 
-                    />
+                <Header currentApp="portfolio" /> 
+                <main>
+                    <Routes>
+                        <Route 
+                            path="/" 
+                            element={
+                                <ProjectPage
+                                    projects={allProjects}
+                                    attributes={allAttributes}
+                                    globalLoading={globalLoading}
+                                />
+                            } 
+                        />
 
-                    <Route 
-                        path="/detail/:descriptiveTitleSlug" 
-                        element={
-                            <ProjectDetailPage
-                                projects={allProjects}
-                                globalLoading={globalLoading}
-                            />
-                        } 
-                    />
+                        <Route 
+                            path="/detail/:descriptiveTitleSlug" 
+                            element={
+                                <ProjectDetailPage
+                                    projects={allProjects}
+                                    globalLoading={globalLoading}
+                                />
+                            } 
+                        />
 
-                    <Route 
-                        path="*" 
-                        element={
-                            <div
-                                className="
-                                    flex flex-col items-center justify-center
-                                    text-gray-700 bg-gray-50 p-4
-                                "
-                                style={{ minHeight: 'calc(100vh - 64px)' }}
-                            >
-                                <h1 className="text-6xl font-extrabold text-red-600">404</h1>
-                                <p className="text-2xl mt-4">Page Not Found</p>
-                                <p className="mt-2 text-lg">The page you are looking for does not exist.</p>
-                                <Link to="/" className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
-                                    Go to Projects Home
-                                </Link>
-                            </div>
-                        } 
-                    />
-                </Routes>
-            </main>
-            <Footer /> 
-        </Router>
+                        <Route 
+                            path="*" 
+                            element={
+                                <div
+                                    className="
+                                        flex flex-col items-center justify-center
+                                        text-gray-700 bg-gray-50 p-4
+                                    "
+                                    style={{ minHeight: 'calc(100vh - 64px)' }}
+                                >
+                                    <h1 className="text-6xl font-extrabold text-red-600">404</h1>
+                                    <p className="text-2xl mt-4">Page Not Found</p>
+                                    <p className="mt-2 text-lg">The page you are looking for does not exist.</p>
+                                    <Link to="/" className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+                                        Go to Projects Home
+                                    </Link>
+                                </div>
+                            } 
+                        />
+                    </Routes>
+                </main>
+                <Footer /> 
+            </Router>
+            <SpeedInsights/>
+        </>
     );
 }
 
